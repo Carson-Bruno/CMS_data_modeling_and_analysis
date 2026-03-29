20dbbe42b033a46c9a82481975e7597a896cfee1
Carson Bruno <122404620+Carson-Bruno@users.noreply.github.com>
2026-03-29 15:40:12 -0400

    Delete load_data.sql

--- load_data.sql
+++
@@ -1,6 +0,0 @@
-
-TRUNCATE TABLE carrier_claims;
-COPY carrier_claims
-FROM 'C:/Users/cbrun/OneDrive/Documents/CMS_synth_proj/raw_data/DE1_0_2008_to_2010_Carrier_Claims_Sample_1A.csv'
-DELIMITER ','
-CSV HEADER;
